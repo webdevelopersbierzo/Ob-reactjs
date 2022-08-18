b568e03a04c7c973d16b071cba2ea9baa87c3288
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-08-18 17:33:36 +0200

    añadido TodoContainer.jsx

---
+++ ejercicio22_23_24/ejercicio24/src/components/containers/TodoContainer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { toggleTodo } from '../../actions/actionsTypes';
+import TodoList from '../pure/TodoList';
+ const filterTodos  = (todos, filter) => {
+    
+    switch (filter) {
+        case 'SHOW_ALL':
+            return todos;
+        
+        case 'SHOW_ACTIVE':
+            return todos.filter((todo)=>!todo.completed);
+        
+        case 'SHOW_COMPLETED':
+            return todos.filter((todo) => todo.completed);
+    
+        default:
+            return todos;
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        todos:filterTodos(state.todosState, state.filterState)
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onTodoClick: (id) =>{
+            dispatch(toggleTodo(id))
+        }
+    }
+}
+const TodosContainer = connect(mapStateToProps, mapDispatchToProps)(TodoList)
+export default TodosContainer
