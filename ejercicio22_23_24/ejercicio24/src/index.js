f3d1a2f4f62f83e16c38081c272bb15d79141696
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-08-18 17:04:44 +0200

    modificado index.js con el provider de redux

--- ejercicio22_23_24/ejercicio24/src/index.js
+++ ejercicio22_23_24/ejercicio24/src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// redux imports
+import {Provider} from 'react-redux';
+import { createAppStore } from './config/storeConfig';
+
+
+// we create app Store
+
+let appStore = createAppStore();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={appStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
